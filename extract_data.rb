27df22ddbdc32eae3a1b0345fae0bc435fb863b8
Florian Richoux <florian.richoux@polytechnique.edu>
2015-05-09 11:17:30 +0200

    Liste des député(e)s qui ont voté oui, plus Gille Savary

--- extract_data.rb
+++ extract_data.rb
@@ -1,27 +1,39 @@
 #!/usr/bin/ruby
+# -*- coding: utf-8 -*-
 
 if ARGV.empty?
   puts "Usage: #{$0} fichier"
   exit
 end
 
+groupe = ""
+
 # pour chaque ligne du fichier
 File.open(ARGV[0]).each do |line|
-  # on parse par les ,
-  words = line.split( ',' )
-
-  # tout en minuscule
-  words[2].downcase!
-  # on supprime des guillemets
-  words[2].gsub!('"', '')
-  # on remplace les espaces par des traits d'union
-  words[2].gsub!(' ', '-')
-
-  words[3].downcase!
-  words[3].gsub!('"', '')
-  words[3].gsub!(' ', '-')
-
-  puts "#{words[2]}-#{words[3]}"
+  if line.start_with?('#')
+    groupe = line[1..-1]
+  else
+    # tout en minuscule
+    line.downcase!
+    # on supprime les fins de ligne
+    line.strip!
+    # on remplace les espaces par des traits d'union
+    line.gsub!(' ', '-')
+    # on supprime les accents
+    line.gsub!('é', 'e')
+    line.gsub!('è', 'e')
+    line.gsub!('ë', 'e')
+    line.gsub!('ê', 'e')
+    line.gsub!('à', 'a')
+    line.gsub!('ä', 'a')
+    line.gsub!('â', 'a')
+    line.gsub!('ö', 'o')
+    line.gsub!('ô', 'o')
+    line.gsub!('ï', 'i')
+    line.gsub!('î', 'i')
+    
+    puts "#{line}_#{groupe}"
+  end
 end
 
 exit
