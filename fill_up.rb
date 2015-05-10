783b714903e5da95f2056fee23e02ac21a633579
Florian Richoux <florian.richoux@polytechnique.edu>
2015-05-10 21:56:40 +0200

    Ca prend forme

--- fill_up.rb
+++ fill_up.rb
@@ -12,6 +12,7 @@ File.open(ARGV[0], "w") do |file|
   file.write("<html>\n")
   file.write("  <head>\n")
   file.write("    <meta charset=\"utf-8\" />\n\n")
+  file.write("    <link rel=\"stylesheet\" href=\"jenevote.css\"/>\n")
   file.write("    <meta name=\"author\" content=\"Canard masqué\" />\n")
   file.write("    <meta name=\"description\"  content=\"La page pour se faire entendre par son/sa député-e\"/>\n")
   file.write("    <meta name=\"keywords\" content=\"PJLRenseignement parlement député vote\" />\n")
@@ -23,17 +24,24 @@ File.open(ARGV[0], "w") do |file|
     line.strip!
     nom_sans_tiret = line.gsub('-', '_')
     nom = line.gsub('-', ' ')
+    nom_capital = nom.split.map(&:capitalize).join(' ')
 
-    file.write("  <img src=\"photos/#{line}.png\" title=\"#{nom}\" width=\"120\" align=\"left\" vspace=\"-50\"/>")
-    file.write("  <script type=\"text/javascript\">\n")
-    file.write("    var clicks_#{nom_sans_tiret} = 0;\n");
-    file.write("    function onClick_#{nom_sans_tiret}() {\n")
-    file.write("      clicks_#{nom_sans_tiret} += 1;\n")
-    file.write("      document.getElementById(\"clicks_#{nom_sans_tiret}\").innerHTML = clicks_#{nom_sans_tiret};\n")
-    file.write("    };\n")
-    file.write("  </script>\n")
-    file.write("  <button type=\"button\" onClick=\"onClick_#{nom_sans_tiret}()\">Je ne vote plus pour vous #{nom_sans_tiret}</button>\n")
-    file.write("  Voix perdues : <a id=\"clicks_#{nom_sans_tiret}\">0</a></br>\n\n")
+    file.write("    <img src=\"photos/#{line}.png\" title=\"#{nom}\"/>\n")
+    file.write("    <script type=\"text/javascript\">\n")
+    file.write("      var clicks_#{nom_sans_tiret} = 0;\n");
+    file.write("      function onClick_#{nom_sans_tiret}() {\n")
+    file.write("        clicks_#{nom_sans_tiret} += 1;\n")
+    file.write("        document.getElementById(\"clicks_#{nom_sans_tiret}\").innerHTML = clicks_#{nom_sans_tiret};\n")
+    file.write("      };\n")
+    file.write("    </script>\n")
+    file.write("    <div class=\"dabox\">\n")
+    file.write("      <div class=\"pos_bouton\">\n")
+    file.write("        <button type=\"button\" onClick=\"onClick_#{nom_sans_tiret}()\">Je ne vote plus pour vous #{nom_capital}</button>\n")
+    file.write("      </div>\n")
+    file.write("      <div class=\"pos_vote\">\n")
+    file.write("        Voix perdues : <a id=\"clicks_#{nom_sans_tiret}\">0</a>\n")
+    file.write("      </div>\n")
+    file.write("    </div>\n\n")
   end
   
   file.write("  </body>\n")
